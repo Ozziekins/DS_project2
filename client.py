1a3f63c67b5699b511878608669768134296871e
Annesard <ryabetsky.andrey@gmail.com>
2020-10-03 22:54:29 +0300

    with blocks

--- client.py
+++ client.py
@@ -9,152 +9,123 @@ from shutil import copyfile, move
 # host = localhost
 # port = 123
 
+def read_from_storage(block_uuid,minion):
+  host,port = minion
+  con=rpyc.connect(host,port=port)
+  minion = con.root.Minion()
+  return minion.get(block_uuid)
+
+
 def init():
 	return 0
 
-def create_file():
+def create_file(path):
 	print("Creating the file ... ")
-	print("Please enter file name ")
-	name = input()
-	print("Please enter the format of file ")
-	format_name = input()
-	file = open(str(name) + "." + str(format_name), "w+")
-	file.close()
+	file = path
 	print("File created") 
 	return 0
 
-def read_file():
-	print("Please enter file name ")
-	name = input()
-	print("Please enter the format of file ")
-	format_name = input()
-	file = open(str(name) + "." + str(format_name), "r")
-	print("Content of the file:")
-	content = file.read()
-	print(content)
-	return 0
 
-def write_file():
+def read_file(name_server, path):
+	#TODO check the name of name_server
+	file_table = name_server.get_file_table_entry(path)
+	if not file_table:
+    	LOG.info("404: file not found")
+    return
+
+    for block in file_table:
+    	# block[1] - server on which it is stored
+    	# block[0] - id of the block
+    	for m in [name_server.get_minions()[_] for _ in block[1]]
+
+def write_file(path, content):
 	print("Writing to file")
-	print("Please enter file name ")
-	name = input()
-	print("Please enter the format of file ")
-	format_name = input()
-	print("Please enter what to write")
-	content = input()
-	file = open(str(name) + "." + str(format_name), "w+")
-	file.write(content)
-	file.close()
+	file = path
+	info = content
 	print("File changed") 
 	return 0
 
-def delete_file():
+def delete_file(path):
 	print("Deleting the file")
-	print("Please enter path to the file ")
-	path= input()
-	os.remove(path)
+	file = path
 	print("File removed") 
 	return 0
 
-def info_file():
+def info_file(path):
 	print("Information about the file")
-	print("Please enter file name ")
-	name = input()
-	print("Please enter the format of file ")
-	format_name = input()
-	info = os.stat(str(name) + "." + str(format_name))
-	print ("file size:", info[ST_SIZE])
-	print ("file modified:", time.asctime(time.localtime(info[ST_MTIME])))
+	file  = path
 	return 0
 
-def copy_file():
+def copy_file(src, dest):
 	print("Copying the file from src to dest ...")
-	print("Please enter the source of the file with format")
-	src = input()
-	print("Please enter the destination of the file with format ")
-	dest = input()
-	copyfile(src, dest)
+	source_file = src
+	destination_file = dest
 	print("File copied") 
 	return 0
 
-def move_file():
+def move_file(src, dest):
 	print("Moving the file from src to dest ...")
-	print("Please enter the source of the file with format")
-	src = input()
-	print("Please enter the destination of the file with format ")
-	dest = input()
-	move(src, dest)
+	source_file = src
+	destination_file = dest
 	print("File moved") 
 	return 0
 
-def open_dir():
+def open_dir(path):
 	print("Changing the directory")
-	print("Please enter the path ")
-	path  = input()
-	os.chdir(path)
+	directory = path
 	print("Directory changed") 
 	return 0
 
-def read_dir():
+def read_dir(path):
 	print("Listing everything in dir ...")
-	print("Please enter the path of the dir")
-	path = input()
-	os.listdir(path)
+	directory - path
 	return 0
 
-def make_dir():
+def make_dir(path):
 	print("Creating the dir...")
-	print("Please enter the path")
-	path = input()
-	os.mkdir( path)
+	directory = path
 	print("Diirectory created")
 	return 0
 
-def delete_dir():
+def delete_dir(path):
 	print("Removing th dir ...")
-	print("Please enter the path")
-	path = input()
-	os.rmdir(path)
+	directory = path
 	print("Dir deleted")
 	return 0
 
-print("Please choose the action:")
-print("1 - initiate the file")
-print("2 - create file")
-print("3 - read file")
-print("4 - write file")
-print("5 - delete file")
-print("6 - info about file")
-print("7 - copy file")
-print("8 - move file")
-print("9 - open directory")
-print("10 - list all files in directory")
-print("11 - create directory")
-print("12 - delete directory")
+# con=rpyc.connect("localhost",port=2131)
+# name_server=con.root.Master()
+menu = sys.argv[1]
 
-menu = int(input())
-
-if menu == 1:
+if menu == 'init':
 	init()
-elif menu == 2:
-	create_file()
-elif menu == 3:
-	read_file()
-elif menu == 4:
-	write_file()
-elif menu == 5:
-	delete_file()
-elif menu == 6:
-	info_file()
-elif menu == 7:
-	copy_file()
-elif menu == 8:
-	move_file()
-elif menu == 9:
-	open_dir()
-elif menu == 10:
-	read_dir()
-elif menu == 11:
-	make_dir()
-elif menu == 12:
-	delete_dir()
+elif menu == 'create_file':
+	create_file(sys.argv[2])
+elif menu == 'read_file':
+	read_file(sys.argv[2])
+elif menu == 'write_file':
+	write_file(sys.argv[2], sys.argv[3])
+elif menu == 'delete_file':
+	delete_file(sys.argv[2])
+elif menu == 'info_file':
+	info_file(sys.argv[2])
+elif menu == 'copy_file':
+	copy_file(sys.argv[2], sys.argv[3])
+elif menu == 'move_file':
+	move_file(sys.argv[2], sys.argv[3])
+elif menu == 'open_dir':
+	open_dir(sys.argv[2])
+elif menu == 'read_dir':
+	read_dir(sys.argv[2])
+elif menu == 'make_dir':
+	make_dir(sys.argv[2])
+elif menu == 'delete_dir':
+	delete_dir(sys.argv[2])
+else:
+  LOG.error("try 'put srcFile destFile OR get file'")
+
+
+
+
+
+
