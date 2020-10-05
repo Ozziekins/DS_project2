be5486afd1409be7953383cd140de0c73ccd7963
Annesard <ryabetsky.andrey@gmail.com>
2020-10-05 19:05:06 +0300

    added info file

--- client.py
+++ client.py
@@ -22,14 +22,15 @@ def read_from_storage(block_uuid,storage):
   return storage.read_file(block_uuid)
 
 
-def init():
-	return 0
+def initialize(naming_server):
+    storage_servers = naming_server.get_storage_servers()
+    for i in range(len(storage_servers)):
+        m = storage_servers['{}'.format(i+1)]
+        initialize_storage_server(m)
 
 # TODO check the argument
 def create_file(name_server, path):
-	print("Creating the file ... ")
-	name_server.create_file(path) 
-	print("File created") 
+	name_server.create_file(path)
 	return 0
 
 
@@ -52,7 +53,6 @@ def read_file(name_server, path):
 
 
 def write_file(name_server, path, content):
-	print("Writing to file")
 	size = os.path.getsize(content)
 	blocks = name_server.write(path, content)
 	with open(data) as f:
@@ -61,11 +61,9 @@ def write_file(name_server, path, content):
 			block_uuid=b[0]
 			storage_servers = [name_server.get_minions()[_] for _ in b[1]]
 			send_to_storage(block_uuid,data,storage_servers)
-	print("File changed") 
 	return 0
 
 def delete_file(name_server, path):
-	print("Deleting the file")
 	file_table = name_server.get_file_table_entry(path)
 	if not file_table:
 		LOG.info("404: file not found")
@@ -77,84 +75,90 @@ def delete_file(name_server, path):
 			con=con.root(host,port=port, config = {"allow_public_attrs" : True})
 			con.root
 			m.delete_file(block[0])
-	if data:
-		print("File removed")
-	else:
-		LOG.info("No blocks found. Possibly a corrupt file")
-	return 0
+	return
 
 def info_file(path):
-	print("Information about the file")
-	file  = path
+	host,port = storage
+	con=con.root(host,port=port, config = {"allow_public_attrs" : True})
+	storage = con.root
+	return storage.file_info(block_uuid)
+
+
+def copy_file(name_server, src, dest):
+
+	return 
+
+def move_file(name_server, src, dest):
+	copy_file(name_server, src, dest)
+	delete_file(name_server, src)
 	return 0
 
-def copy_file(src, dest):
-	print("Copying the file from src to dest ...")
-	source_file = src
-	destination_file = dest
-	print("File copied") 
-	return 0
-
-def move_file(src, dest):
-	print("Moving the file from src to dest ...")
-	source_file = src
-	destination_file = dest
-	print("File moved") 
-	return 0
-
-def open_dir(path):
-	print("Changing the directory")
-	directory = path
-	print("Directory changed") 
+def open_dir(name_server, path):
+	
 	return 0
 
 def read_dir(path):
-	print("Listing everything in dir ...")
-	directory - path
+
 	return 0
 
 def make_dir(name_server, path):
-	print("Creating the dir...")
 	name_server.create_file(path)
-	print("Diirectory created")
 	return 0
 
 def delete_dir(path):
-	print("Removing th dir ...")
 	directory = path
-	print("Dir deleted")
 	return 0
 
-con=rpyc.connect("127.0.0.1",port=2000, config = {"allow_public_attrs" : True})
-name_server=con.root
-menu = sys.argv[1]
+def main(args):
+    con=rpyc.connect("127.0.0.1",port=2000, config = {"allow_public_attrs" : True})
+    naming_server=con.root
 
-if menu == 'init':
-	init()
-elif menu == 'create_file':
-	create_file(name_server,sys.argv[2])
-elif menu == 'read_file':
-	read_file(name_server, sys.argv[2])
-elif menu == 'write_file':
-	write_file(name_server, sys.argv[2], sys.argv[3])
-elif menu == 'delete_file':
-	delete_file(name_server, sys.argv[2])
-elif menu == 'info_file':
-	info_file(name_server, sys.argv[2])
-elif menu == 'copy_file':
-	copy_file(name_server, sys.argv[2], sys.argv[3])
-elif menu == 'move_file':
-	move_file(name_server, sys.argv[2], sys.argv[3])
-elif menu == 'open_dir':
-	open_dir(name_server, sys.argv[2])
-elif menu == 'read_dir':
-	read_dir(name_server, sys.argv[2])
-elif menu == 'make_dir':
-	make_dir(name_server, sys.argv[2])
-elif menu == 'delete_dir':
-	delete_dir(name_server, sys.argv[2])
-else:
-  LOG.error("try 'put srcFile destFile OR get file'")
+    menu = sys.argv[1]
+
+    if menu == 'init':
+        initialize(naming_server)
+    elif menu == 'create':
+        create_file(naming_server, sys.argv[2])
+    elif menu == 'read':
+        read_file(naming_server, sys.argv[2])
+    elif menu == 'write':
+        write_file(naming_server, sys.argv[2], sys.argv[3])
+    elif menu == 'delete':
+        delete_file(naming_server, sys.argv[2])
+    elif menu == 'info':
+        info_file(naming_server, sys.argv[2])
+    elif menu == 'copy':
+        copy_file(naming_server, sys.argv[2], sys.argv[3])
+    elif menu == 'move':
+        move_file(naming_server, sys.argv[2], sys.argv[3])
+    elif menu == 'opndir':
+        open_dir(naming_server, sys.argv[2])
+    elif menu == 'rddir':
+        read_dir(naming_server, sys.argv[2])
+    elif menu == 'mkdir':
+        make_dir(naming_server, sys.argv[2])
+    elif menu == 'dltdir':
+        delete_dir(naming_server, sys.argv[2])
+    else:
+        print("Possible commands")
+        print("-------------------------")
+        print("init")
+        print("create <file_name>")
+        print("read <file_name>")
+        print("write <src_file> <dest_file>")
+        print("delete <file_name>")
+        print("info <file_name>")
+        print("copy src_file dest_file")
+        print("move src dest")
+        print("opndir directory/")
+        print("rddir directory/")
+        print("mkdir directory/")
+        print("dltdir directory/")
+        LOG.error("incorrect command")
+
+
+if __name__ == "__main__":
+    main(sys.argv[1:])
 
 
 
---
+++ name_server.py
@@ -0,0 +1,72 @@
+import rpyc
+import uuid
+import threading 
+import math
+import random
+import signal
+import pickle
+import sys
+import os
+import pprint
+
+from rpyc.utils.server import ThreadedServer
+
+BLOCK_SIZE = 128
+REPLICATION_FACTOR = 2
+MINIONS = {"1":("127.0.0.1", 5555), "2":("127.0.0.1",6666)}
+
+class NamingService(rpyc.Service):
+    file_table = {}
+    block_mapping = {}
+    storage_servers = MINIONS
+
+    block_size = BLOCK_SIZE
+    replication_factor = REPLICATION_FACTOR
+
+    def exposed_read(self,fname):
+      mapping = self.__class__.file_table[fname]
+      return mapping
+
+    def exposed_write(self,dest,size):
+      if self.exists(dest):
+        pass # ignoring for now, will delete it later
+
+      self.__class__.file_table[dest]=[]
+
+      num_blocks = self.calc_num_blocks(size)
+      blocks = self.alloc_blocks(dest,num_blocks)
+      return blocks
+
+    def exposed_get_file_table_entry(self,fname):
+      if fname in self.__class__.file_table:
+        return self.__class__.file_table[fname]
+      else:
+        return None
+
+    def exposed_get_block_size(self):
+      return self.__class__.block_size
+
+    def exposed_get_storage_servers(self):
+      return self.__class__.storage_servers
+
+    def calc_num_blocks(self,size):
+      return int(math.ceil(float(size)/self.__class__.block_size))
+
+    def exists(self,file):
+      return file in self.__class__.file_table
+
+    def alloc_blocks(self,dest,num):
+        blocks = []
+        for _ in range(0,num):
+            block_uuid = uuid.uuid1()
+            nodes_ids = random.sample(self.__class__.storage_servers.keys(),self.__class__.replication_factor)
+            blocks.append((block_uuid,nodes_ids))
+
+            self.__class__.file_table[dest].append((block_uuid,nodes_ids))
+        pprint.pprint(self.__class__.file_table)
+        return blocks
+
+
+if __name__ == "__main__":
+  t = ThreadedServer(NamingService, port = 2000, protocol_config = {"allow_public_attrs" : True})
+  t.start() 
